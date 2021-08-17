7005e6c8d117942985b9ce27d21263c968a7a821
3i-19-16 <aiden.seo@3i.ai>
2021-08-17 09:45:33 +0900

    init

--- wasm/wasm.go
+++ wasm/wasm.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 	"syscall/js"
+	"time"
 )
 
 func add(this js.Value, i []js.Value) interface{} {
@@ -19,8 +21,29 @@ func add(this js.Value, i []js.Value) interface{} {
 	return js.ValueOf(int1 + int2)
 }
 
+
+func calculateGo(this js.Value, i []js.Value) interface{} {
+	start := time.Now()
+	number := i[0].Int()
+	result := []int{0, 1}
+	if number == 0 {
+		fmt.Printf("%d\n", 0)
+	}
+	if number == 1 {
+		fmt.Printf("%d,%d\n", 0, 1)
+	}
+	for i := 2; i < number; i++ {
+		result = append(result, result[i-2] + result[i-1])
+	}
+	fmt.Println("calculateGo:", result)
+	elapsed := time.Since(start)
+	log.Println("calculateGo:", elapsed)
+	return js.ValueOf(0)
+}
+
 func registerCallbacks() {
 	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("calculateGo", js.FuncOf(calculateGo))
 }
 
 func main() {
